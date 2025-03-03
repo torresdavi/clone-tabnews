74637c8baa69260e0197f139868ba1a4445f1e09
Davi Torres <torresdavi04@gmail.com>
2025-03-03 21:08:10 +0000

    adiciona o script de formatacao `lint:check e lint:fix` ao projeto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do tabnews para fins de aprendizado
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
-function Home(){
-  return <h1>Minha home, aprendendo a ser um novo programador! Dias melhores virão Beatriz, eu te amo lindeza!</h1>
-
+function Home() {
+  return (
+    <h1>
+      Minha home, aprendendo a ser um novo programador! Dias melhores virão
+      Beatriz, eu te amo lindeza!
+    </h1>
+  );
 }
 
 export default Home;
