7e6f680f0e2bca7d10baa9a27cf8ce357dd78dc5
Davi Torres <torresdavi04@gmail.com>
2025-05-15 22:32:35 +0000

    Add `updated_at`, `pg_version`,` max_connections` and `active connections` to status endpoint. Create unit tests.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  response.status(200).json({ eu: "sou o máximo", voce: "não é o máximo" });
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const pgVersion = await database.query("SELECT version();");
+  const pgMaxConnections = await database.query("SHOW max_connections;");
+  const pgActiveConnections = await database.query(
+    "SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active'",
+  );
+
+  const pgVersionString = pgVersion.rows[0]["version"];
+  const parsedPgMaxConnections = parseInt(
+    pgMaxConnections.rows[0]["max_connections"],
+    10,
+  );
+
+  const parsedActiveConnections = parseInt(
+    pgActiveConnections.rows[0]["count"],
+    10,
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    pg_version: pgVersionString,
+    pg_max_connections: parsedPgMaxConnections,
+    pg_active_connections: parsedActiveConnections,
+  });
 }
 
 export default status;
+
+// Desafio: Retornar Versão do Postgres, Qtd de conexões máximas e conexões usadas
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,37 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  //console.log(responseBody);
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+});
+
+test("GET to /api/v1/status should return Postgres Version", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(responseBody.pg_version).toBeDefined();
+  expect(responseBody.pg_version).toEqual(
+    "PostgreSQL 16.0 on x86_64-pc-linux-musl, compiled by gcc (Alpine 12.2.1_git20220924-r10) 12.2.1 20220924, 64-bit",
+  );
+});
+
+test("GET to /api/v1/status should return Postgres Max Connections", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(responseBody.pg_max_connections).toBeDefined();
+  expect(responseBody.pg_max_connections).toBeGreaterThan(0);
+  expect(responseBody.pg_max_connections).toEqual(100);
+});
+
+test("GET to /api/v1/status should return Postgres Active Connections", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(responseBody.pg_active_connections).toBeDefined();
+  expect(responseBody.pg_active_connections).toBeGreaterThan(0);
 });
