0c4fcd505b6f6582666951eb7bd43ca35bfac34c
Davi Torres <torresdavi04@gmail.com>
2025-05-30 18:52:54 +0000

    make ssl configuration more robust. add ca.

--- infra/database.js
+++ infra/database.js
@@ -8,7 +8,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSslValues(),
   });
 
   console.log("Production Credentials:", {
@@ -38,3 +38,12 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSslValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? false : true;
+}
