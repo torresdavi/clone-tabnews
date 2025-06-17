51469ccd6b211164070dc9ce500ba005aa65e46f
Davi Torres <torresdavi04@gmail.com>
2025-06-17 22:44:57 +0000

    fix migrations endpoint connection bug.

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,44 +3,46 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    // returns 405 to not allowed for the others requests
+    return response.status(405).json({
+      error: `Method ${request.method} not allowed`,
+    });
+  }
 
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  let dbClient;
 
   try {
-    switch (request.method) {
-      case "GET":
-        const pendingMigrations = await migrationRunner(
-          defaultMigrationOptions,
-        );
-        return response.status(200).json(pendingMigrations);
-        break;
-      case "POST":
-        // using spread to retrieve values from defaultMigrationOptions
-        // and overwrite dryRun value
-        const migratedMigrations = await migrationRunner({
-          ...defaultMigrationOptions,
-          dryRun: false,
-        });
+    dbClient = await database.getNewClient();
 
-        if (migratedMigrations.length > 0) {
-          return response.status(201).json(migratedMigrations);
-          break;
-        }
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
 
-        return response.status(200).json(migratedMigrations);
-        break;
-      default:
-        // returns 405 to not allowed for the others requisitions
-        return response.status(405).json({ error: "not allowed." });
-        break;
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      return response.status(200).json(pendingMigrations);
+    }
+
+    if (request.method === "POST") {
+      // using spread to retrieve values from defaultMigrationOptions
+      // and overwrite dryRun value
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+
+      return response.status(200).json(migratedMigrations);
     }
   } catch (error) {
     console.log(error);
