e1829661e9635419e5f2a2fff8c8b90b1225adc2
Davi Torres <torresdavi04@gmail.com>
2025-07-07 23:41:27 +0000

    rollback node version. insert breaking changes on api/v1/status endpoint.

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -6,7 +6,7 @@ beforeAll(async () => {
 });
 
 test("GET to /api/v1/status should return status 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+  const response = await fetch("http://localhost:3000/api/v1/statuss");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
