6c3562c88f4b518fa361b5cc7949cb83cf0b9cb0
Davi Torres <torresdavi04@gmail.com>
2025-07-12 15:13:02 +0000

    style: format with eslint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -5,37 +5,37 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return status 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /status", () => {
+  test("GET to /api/v1/status should return status 200", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  //console.log(responseBody);
+    const responseBody = await response.json();
+    const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+    expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  });
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-});
+  test("GET to /api/v1/status should return Postgres Version", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const responseBody = await response.json();
 
-test("GET to /api/v1/status should return Postgres Version", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
+    expect(responseBody.dependencies.database.version).toEqual("16.0");
+  });
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-});
+  test("GET to /api/v1/status should return Postgres Max Connections", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const responseBody = await response.json();
 
-test("GET to /api/v1/status should return Postgres Max Connections", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
+    expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  });
 
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-});
+  test("GET to /api/v1/status should return Postgres Active Connections", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const responseBody = await response.json();
 
-test("GET to /api/v1/status should return Postgres Active Connections", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
-
-  expect(responseBody.dependencies.database.active_connections).toBeGreaterThan(
-    0,
-  );
-  expect(responseBody.dependencies.database.active_connections).toEqual(1);
+    expect(
+      responseBody.dependencies.database.active_connections,
+    ).toBeGreaterThan(0);
+    expect(responseBody.dependencies.database.active_connections).toEqual(1);
+  });
 });
