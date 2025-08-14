90aa6facd6b45f3cf823e9eb1f2f357ef82a2014
Davi Torres <torresdavi04@gmail.com>
2025-08-14 23:33:29 +0000

    test: add `invalid data` test case

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -10,7 +10,7 @@ beforeAll(async () => {
 
 describe("PATCH api/v1/users/[username]", () => {
   describe("Anonymous user", () => {
-    test("Updating username and email", async () => {
+    test("Updating with valid data", async () => {
       // Populating users db
       const response1 = await fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
@@ -56,6 +56,48 @@ describe("PATCH api/v1/users/[username]", () => {
       expect(uuidVersion(response2Body.id)).toBe(4);
       expect(Date.parse(response2Body.created_at)).not.toBeNaN();
       expect(Date.parse(response2Body.updated_at)).not.toBeNaN();
+    });
+
+    test("Updating with invalid data", async () => {
+      // Populating users db
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "thirdUserName",
+          email: "thirdUserEmail@gmail.com",
+          password: "123456",
+        }),
+      });
+
+      expect(response1.status).toBe(201);
+
+      // Retrieving user
+      const response2 = await fetch(
+        "http://localhost:3000/api/v1/users/thirdUserName",
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: "secondUserName",
+            email: "thirdUserEmail@gmail.com",
+          }),
+        },
+      );
+
+      const response2Body = await response2.json();
+
+      expect(response2.status).toBe(400);
+      expect(response2Body).toEqual({
+        name: "ValidationError",
+        message: "O e-mail informado já está sendo utilizado.",
+        action: "Utilize outro e-mail para realizar o cadastro.",
+        status_code: 400,
+      });
     });
   });
 });
